d8c493dc9c555c7eb195fef6299515e1c918c437
Lulapalula <isabella.ramirez0704@gmail.com>
2023-09-12 13:22:51 -0600

    jelp

---
+++ nombreDeLuchador-20230912T182535Z-001/nombreDeLuchador/nombreDeLuchador/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nombreDeJugador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneradorDeNombres int1 = new GeneradorDeNombres();//invocacion de la clase
+            int1.generar(6,2);//invocacion del metodo
+            Console.ReadLine();
+        }
+
+    }
+
+}
