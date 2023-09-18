8cd49b2cee64bb5832cca51361e99ec5a1cf0ca5
Lulapalula <isabella.ramirez0704@gmail.com>
2023-09-18 12:12:32 -0600

    tareanso

---
+++ BancoCuenta/BancoCuenta/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class CuentaBancaria
+{
+    // Atributos privados
+    private int saldo;            // Almacena el saldo de la cuenta bancaria como un entero.
+    private int pin;              // Almacena el PIN de la cuenta bancaria como un entero.
+    private bool pinEstablecido;  // Indica si el PIN de la cuenta bancaria está establecido.
+
+    // Constructor
+    public CuentaBancaria()
+    {
+        saldo = 0;               // Inicializa el saldo en 0 cuando se crea una instancia de la cuenta.
+        pinEstablecido = false;  // Inicializa la variable que indica si el PIN está establecido en "false".
+    }
+
+    // Propiedad para el saldo
+    public int Saldo
+    {
+        get
+        {
+            if (!pinEstablecido)
+            {
+                Console.WriteLine("El PIN no está establecido. No se puede acceder al saldo.");
+                return 0;
+            }
+            return saldo;  // Devuelve el valor del saldo si el PIN está establecido.
+        }
+        set
+        {
+            if (pinEstablecido)
+            {
+                saldo = value;  // Establece el valor del saldo si el PIN está establecido.
+            }
+            else
+            {
+                Console.WriteLine("El PIN no está establecido. No se puede cambiar el saldo.");
+            }
+        }
+    }
+
+    // Método para establecer el PIN
+    public void EstablecerPIN(int nuevoPIN)
+    {
+        pin = nuevoPIN;           // Asigna el nuevo PIN proporcionado al atributo "pin".
+        pinEstablecido = true;    // Marca que el PIN está establecido como "true".
+    }
+}
--- BancoCuenta/BancoCuenta/Program.cs
+++ BancoCuenta/BancoCuenta/Program.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BancoCuenta
 {
-    class CuentaBancaria
+    class Program
     {
-        
+        static void Main(string[] args)
+        {
+            // Crear una instancia de la clase CuentaBancaria
+            CuentaBancaria miCuenta = new CuentaBancaria();
 
+            // Establecer el PIN
+            miCuenta.EstablecerPIN(); 
+
+            // Acceder al saldo (esto funcionará porque se estableció el PIN)
+            int saldoActual = miCuenta.Saldo;
+            Console.WriteLine($"Saldo actual: {saldoActual} dólares");
+
+            // Intentar cambiar el saldo (esto mostrará un mensaje de error si el PIN no está establecido)
+            miCuenta.Saldo = 1000; // Cambia el saldo si se estableció el PIN
+
+            // Acceder al saldo nuevamente
+            saldoActual = miCuenta.Saldo;
+            Console.WriteLine($"Saldo actual: {saldoActual} dólares");
+
+            Console.ReadLine(); // Esperar a que el usuario presione una tecla antes de cerrar la consola
+        }
     }
 }
