92aaa9b577a38ff1323e33e84fc798924cc435a8
Lulapalula <isabella.ramirez0704@gmail.com>
2023-09-29 11:51:45 -0600

    clase 29/09/23
    aaaaaa

---
+++ Plataformero/Assets/Scripts/Personaje.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public int hp = 70;
+    public int hpMax = 100;
+    public int vidas = 3;
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
